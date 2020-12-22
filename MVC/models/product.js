65c94e22d51ff40dad229e212dd42aa4e043059f
Leon Michalak <leonmichalak6@gmail.com>
2020-12-22 13:25:15 +0000

    Refactored product model code to use DRY architecture

--- MVC/models/product.js
+++ MVC/models/product.js
@@ -2,20 +2,26 @@ const fs = require("fs");
 const path = require("path");
 const root_path = require("../util/path");
 
+const f = path.join(root_path, "data", "products.json");
+
+function get_products_from_file(cb) {
+  fs.readFile(f, (err, file_content) => {
+    if (err) {
+      console.log(err, "fetching data error");
+      return cb([]);
+    }
+
+    cb(JSON.parse(file_content));
+  });
+}
+
 module.exports = class Product {
   constructor(title) {
     this.title = title;
   }
 
   save() {
-    const f = path.join(root_path, "data", "products.json");
-    fs.readFile(f, (err, file_content) => {
-      let products = [];
-
-      if (!err) {
-        products = JSON.parse(file_content);
-      }
-
+    get_products_from_file((products) => {
       products.push(this);
       fs.writeFile(f, JSON.stringify(products), (err) => {
         console.log(err);
@@ -24,15 +30,6 @@ module.exports = class Product {
   }
 
   static fetchAll(cb) {
-    const f = path.join(root_path, "data", "products.json");
-
-    fs.readFile(f, (err, file_content) => {
-      if (err) {
-        console.log(err, "fetching data error");
-        cb([]);
-      }
-
-      cb(JSON.parse(file_content));
-    });
+    get_products_from_file(cb);
   }
 };
