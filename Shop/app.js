a8728c895f71abcf79e2876067fe2dcd9792225e
Leon Michalak <leonmichalak6@gmail.com>
2021-01-07 19:19:06 +0000

    MySQL Connection established

--- Shop/app.js
+++ Shop/app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const db = require("./util/database");
 const app = express();
 
 app.set("view engine", "ejs");
@@ -12,9 +13,20 @@ const admin_routes = require("./routes/admin");
 const shop_routes = require("./routes/shop");
 const unmatched_route_controller = require("./controllers/unmatched_route");
 
+// Database
+db.execute("SELECT * FROM products")
+  .then((result) => {
+    console.log(result[0]);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// External middlewares
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+// Route middlewares
 app.use("/admin", admin_routes.routes);
 app.use(shop_routes.routes);
 
