e1ce7a8545476c8e35bf3bdb75991c662970353c
Leon Michalak <leonmichalak6@gmail.com>
2021-01-07 19:36:01 +0000

    Database connection code established

--- Shop/util/database.js
+++ Shop/util/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
   host: "localhost",
   user: "root",
   database: "node_shop",
-  password: "...",
+  password: process.env["database_password"],
 });
 
 module.exports = pool.promise();
