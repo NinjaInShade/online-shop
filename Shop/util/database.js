9dfe1d85420ffe65a0d2f66ad6d30f575823d291
Leon Michalak <leonmichalak6@gmail.com>
2021-01-07 19:33:54 +0000

    Testing env file

--- Shop/util/database.js
+++ Shop/util/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
   host: "localhost",
   user: "root",
   database: "node_shop",
-  password: "xdminecrafter99",
+  password: "...",
 });
 
 module.exports = pool.promise();
