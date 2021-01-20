5383510b97c74ae7dcae6928bf464746d5376b97
Leon Michalak <leonmichalak6@gmail.com>
2021-01-20 13:42:18 +0000

    Checkout button now creates an order with the current order items from whatever was in their cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,4 @@
 const Product = require("../models/product");
-const Cart = require("../models/cart");
 
 // GET Reqs
 function get_index(req, res, next) {
@@ -108,7 +107,28 @@ function post_remove_cart(req, res, next) {
 }
 
 function post_create_order(req, res, next) {
-  res.redirect("/checkout");
+  req.user
+    .getCart()
+    .then((cart) => {
+      return cart.getProducts();
+    })
+    .then((products) => {
+      return req.user
+        .createOrder()
+        .then((order) => {
+          return order.addProducts(
+            products.map((product) => {
+              product.orderitem = { quantity: product.cartitem.quantity };
+              return product;
+            })
+          );
+        })
+        .catch((err) => console.log(err));
+    })
+    .then(() => {
+      res.redirect("/orders");
+    })
+    .catch((err) => console.log(err));
 }
 
 module.exports = { get_index, get_cart, get_checkout, get_orders, post_cart, post_remove_cart, post_create_order };
