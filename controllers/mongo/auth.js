b33baff19e7f0ac398d8f49184fbb76f50814ecf
Leon Michalak <leonmichalak6@gmail.com>
2021-02-01 13:29:02 +0000

    Basic signin flow done

--- controllers/mongo/auth.js
+++ controllers/mongo/auth.js
@@ -18,18 +18,37 @@ function get_signup(req, res, next) {
 }
 
 function post_login(req, res, next) {
-  // Handle login, set all auth states and metadata then redirect to some page.
-  User.findById("6014513725c460218c999ff1")
-    .then((user) => {
-      req.session.is_authenticated = true;
-      req.session.user = user;
-      req.session.save((err) => {
-        if (err) {
-          console.log(err);
-        }
+  // Find user, if user found login (if not redirect and send error msg back to view), validate and set all auth states and metadata then redirect to some page.
+  const email = req.body.email;
+  const password = req.body.password;
 
-        res.redirect("/");
-      });
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        return res.redirect("/auth/login");
+      }
+
+      bcrypt
+        .compare(password, user.password)
+        .then((do_match) => {
+          if (do_match) {
+            req.session.is_authenticated = true;
+            req.session.user = user;
+            return req.session.save((err) => {
+              if (err) {
+                console.log(err);
+              }
+
+              res.redirect("/");
+            });
+          }
+
+          return res.redirect("/auth/login");
+        })
+        .catch((err) => {
+          console.log(err);
+          req.redirect("/auth/login");
+        });
     })
     .catch((err) => console.log(err));
 }
@@ -48,17 +67,16 @@ function post_signup(req, res, next) {
       // if exists redirect back to signup form with error msgs.
       if (user) {
         return res.redirect("/auth/signup");
-      } else {
-        // If doesn't, create a new user in db - hash password first.
-        return bcrypt
-          .hash(password, 12)
-          .then((hashed_password) => {
-            const new_user = new User({ name, email, password: hashed_password, cart: { items: [] } });
-
-            return new_user.save();
-          })
-          .catch((err) => console.log(err));
       }
+      // If doesn't, create a new user in db - hash password first.
+      return bcrypt
+        .hash(password, 12)
+        .then((hashed_password) => {
+          const new_user = new User({ name, email, password: hashed_password, cart: { items: [] } });
+
+          return new_user.save();
+        })
+        .catch((err) => console.log(err));
     })
     .then((result) => {
       // then redirect to login page for user to login.
